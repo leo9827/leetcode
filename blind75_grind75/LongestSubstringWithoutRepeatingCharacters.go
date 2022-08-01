6f8644b950c4a5ad4481e51e7b82b6f0f4c207be
hawksli <hawksli@tencent.com>
2022-08-01 12:24:22 +0800

    update

---
+++ blind75_grind75/LongestSubstringWithoutRepeatingCharacters.go
@@ -0,0 +1,26 @@
+package main
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) < 2 {
+		return len(s)
+	}
+	maxlen := 0
+	table := map[byte]int{}
+	for i, j := 0, 0; j < len(s); j++ {
+		c := s[j]
+
+		if table[c] != 0 && table[c] >= i {
+			i = table[c] + 1
+		}
+		maxlen = max(maxlen, j-i+1)
+		table[c] = j
+	}
+	return maxlen
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
