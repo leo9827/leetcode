72009158d9d95dc02d5050d8fd313ddb40d10e23
hawksli <hawksli@tencent.com>
2022-08-05 14:35:04 +0800

    update

---
+++ blind75_grind75/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,51 @@
+package grind75;
+
+import java.util.Stack;
+
+public class RemoveNthNodeFromEndofList {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode();
+        start.next = head;
+        ListNode slow = start, fast = start;
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return start.next;
+    }
+
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+        if (head == null) return null;
+        if (head.next == null && n == 1) return null;
+
+        // 使用栈
+        Stack<ListNode> stack = new Stack<>();
+
+        ListNode cur = head;
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.next;
+        }
+        if (n > stack.size()) return head;
+        ListNode left, right = null;
+        while (n > 1) {
+            right = stack.pop();
+            n--;
+        }
+        stack.pop();
+        if (stack.isEmpty()) {
+            return right;
+        }
+
+        left = stack.pop();
+        left.next = right;
+
+        return head;
+    }
+}
