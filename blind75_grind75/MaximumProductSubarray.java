3566f5cba5957f332a539272b8aadb38d18509fe
hawksli <hawksli@tencent.com>
2022-08-12 11:26:28 +0800

    update

--- blind75_grind75/MaximumProductSubarray.java
+++ blind75_grind75/MaximumProductSubarray.java
@@ -1,29 +1,28 @@
 package grind75;
 
+import java.util.Arrays;
+
 public class MaximumProductSubarray {
 
     public static void main(String[] args) {
         MaximumProductSubarray su = new MaximumProductSubarray();
         System.out.println(su.maxProduct(new int[]{2,3,-2,4}));  //6
-        System.out.println(su.maxProduct(new int[]{-2,0,-1}));      //0
-        System.out.println(su.maxProduct(new int[]{-3,-1,-1}));     //3
+        System.out.println(su.maxProduct(new int[]{-2,0,-1,-2}));   //0
+        System.out.println(su.maxProduct(new int[]{-3,-1,-1}));  //3
     }
     public int maxProduct(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        if (nums.length < 2) return nums[0];
-        /*
-        nums length >= 1
-        case1: [2, 3, -2, 4]
-                root
-        [2,3]   [3,-2,4]
-               [3,-2], [-2,4]
+        if (nums.length == 1) return nums[0];
 
-        case2: [-3,-1,-1]
-        case3: [-3]
-        case3: [-3,-1]
-         */
+        int res = nums[0];
+        int max = 1;
+        int min = 1;
 
-
-        return 1;
+        for (int n : nums) {
+            int tmp = max * n;
+            max = Math.max(n, Math.max(tmp, min * n));
+            min = Math.min(n, Math.min(tmp, min * n));
+            res = Math.max(res, max);
+        }
+        return res;
     }
 }
