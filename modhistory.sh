cad3d4f9c99bbcad4561dc41c07fe892721ec497
leo9827 <741829374@qq.com>
2024-01-09 10:17:32 +0800

    modify layout

---
+++ modhistory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export FILTER_BRANCH_SQUELCH_WARNING=1 # This will suppress the warning shown by git
+
+git filter-branch --commit-filter '
+        if [ "$GIT_AUTHOR_EMAIL" = "lihong98@jd.com" ];
+        then
+                GIT_AUTHOR_NAME="leo9827";
+                GIT_AUTHOR_EMAIL="741829374@qq.com";
+                git commit-tree "$@";
+        else
+                git commit-tree "$@";
+        fi' HEAD
