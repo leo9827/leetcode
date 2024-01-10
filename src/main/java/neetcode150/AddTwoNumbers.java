ae6f7653cdf7142060256c549fa0a8771b4cc8dc
leo9827 <741829374@qq.com>
2024-01-10 21:15:51 +0800

    add problem.2 solution

---
+++ src/main/java/neetcode150/AddTwoNumbers.java
@@ -0,0 +1,56 @@
+package neetcode150;
+
+/**
+ * 2. Add Two Numbers
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order, and each of their nodes contains a single digit.
+ * Add the two numbers and return the sum as a linked list.
+ * <p>
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Example 1:
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * <p>
+ * Example 2:
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ * <p>
+ * Example 3:
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in each linked list is in the range [1, 100].
+ * 0 <= Node.val <= 9
+ * It is guaranteed that the list represents a number that does not have leading zeros.
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int sum = 0;
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while (l1 != null || l2 != null) {
+            sum = sum / 10; // 0 or 1 ( init by last sum)
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            cur.next = new ListNode(sum % 10); // 保留个位数 sum % 10
+            cur = cur.next;
+        }
+        if (sum / 10 == 1) { // last sum is over 10? 满 10 进1
+            cur.next = new ListNode(1);
+        }
+        return dummy.next;
+    }
+}
+
