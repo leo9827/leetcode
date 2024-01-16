74991daf42634dc171d3c1f1124d1aabe1f67588
leo9827 <741829374@qq.com>
2024-01-16 21:46:38 +0800

    update solution

--- src/main/java/neetcode150/array/ContainsDuplicate.java
+++ src/main/java/neetcode150/array/ContainsDuplicate.java
@@ -2,12 +2,35 @@ package neetcode150.array;
 
 import java.util.HashSet;
 
-// https://leetcode.com/problems/contains-duplicate/
+/**
+ * <a href="https://leetcode.com/problems/contains-duplicate/">217存在重复元素</a>.
+ * 简单
+ * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3,1]  <p>
+ * 输出：true <p>
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,3,4] <p>
+ * 输出：false <p>
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1,1,1,3,3,4,3,2,4,2] <p>
+ * 输出：true
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ */
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
-        for(int i=0;i<nums.length-1;i++)
-        {
-            if(nums[i]==nums[i+1])
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1])
                 return true;
         }
         HashSet<Integer> set = new HashSet<>();
--- src/main/java/neetcode150/array/LongestConsecutiveSequence.java
+++ src/main/java/neetcode150/array/LongestConsecutiveSequence.java
@@ -4,8 +4,60 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-// https://leetcode.com/problems/longest-consecutive-sequence/
+
+/**
+ * <a href="https://leetcode.com/problems/longest-consecutive-sequence/">128. 最长连续序列</a>
+ * 中等
+ * <p>
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ * <p>
+ * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ * <p>
+ * 示例 1：  <p>
+ * 输入：nums = [100,4,200,1,3,2] <p>
+ * 输出：4 <p>
+ * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。 <p>
+ * <p>
+ * 示例 2： <p>
+ * <p>
+ * 输入：nums = [0,3,7,2,5,8,4,6,0,1] <p>
+ * 输出：9 <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= nums.length <= 105 <p>
+ * -109 <= nums[i] <= 109
+ */
 public class LongestConsecutiveSequence {
+
+    public static void main(String[] args) {
+        int[] case1 = new int[]{100, 4, 200, 1, 3, 2};
+        System.out.println(new Solution().longestConsecutive(case1) == 4);
+    }
+
+    static class Solution {
+
+        public int longestConsecutive(int[] nums) {
+            if (nums.length <= 1) return nums.length;
+            Set<Integer> tb = new HashSet<>();
+            for (int n : nums) tb.add(n);
+
+            int max = 0;
+            for (int n : nums) {
+                // 因为是连续的，所以不论从哪个位置开始，都可以向左移动和向右移动来到达连续范围的起点和终点
+                int toMin = n - 1;
+                int toMax = n + 1;
+                while (tb.remove(toMin)) toMin--;
+                while (tb.remove(toMax)) toMax++;
+                max = Math.max(max, toMax - toMin - 1);
+                if (tb.isEmpty()) return max;
+            }
+
+            return max;
+        }
+    }
+
     public int longestConsecutive(int[] nums) {
         if (nums.length < 2) return nums.length;
         Set<Integer> table = new HashSet<>(nums.length);
@@ -17,7 +69,7 @@ public class LongestConsecutiveSequence {
             while (table.remove(left)) left--;
             while (table.remove(right)) right++;
             ans = Math.max(ans, right - left - 1);
-            if (table.isEmpty()) return ans;//save time if there are items in nums, but no item in hashset.
+            if (table.isEmpty()) return ans; // save time if there are items in nums, but no item in hashset.
         }
         return ans;
     }
