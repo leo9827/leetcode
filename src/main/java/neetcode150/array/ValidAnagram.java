f07c694484719c116f79b33c027e7287834f0b25
leo9827 <741829374@qq.com>
2024-01-16 22:25:15 +0800

    update solution

--- src/main/java/neetcode150/array/ValidAnagram.java
+++ src/main/java/neetcode150/array/ValidAnagram.java
@@ -1,6 +1,25 @@
 package neetcode150.array;
 
-// https://leetcode.com/problems/valid-anagram/
+/**
+ * <a href="https://leetcode.com/problems/valid-anagram/">242. 有效的字母异位词</a>  <p>
+ * 简单 <p>
+ * <p>
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * <p>
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ * <p>
+ * 示例 1: <p>
+ * 输入: s = "anagram", t = "nagaram" <p>
+ * 输出: true <p>
+ * <p>
+ * 示例 2: <p>
+ * 输入: s = "rat", t = "car" <p>
+ * 输出: false <p>
+ * <p>
+ * 提示:
+ * 1 <= s.length, t.length <= 5 * 104 <p>
+ * s 和 t 仅包含小写字母
+ */
 public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
         // 1.hashset
