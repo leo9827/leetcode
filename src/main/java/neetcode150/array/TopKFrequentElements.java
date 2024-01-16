fa6cd5c7dc8a4a9ca071c02e55a328c6f3de360c
leo9827 <741829374@qq.com>
2024-01-16 22:23:06 +0800

    update solution

--- src/main/java/neetcode150/array/TopKFrequentElements.java
+++ src/main/java/neetcode150/array/TopKFrequentElements.java
@@ -1,9 +1,30 @@
 package neetcode150.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-// https://leetcode.com/problems/top-k-frequent-element/
+/**
+ * <a href="https://leetcode.com/problems/top-k-frequent-element/"> 347. 前 K 个高频元素 </a>
+ * 中等
+ * <p>
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+ * <p>
+ * 示例 1: <p>
+ * 输入: nums = [1,1,1,2,2,3], k = 2 <p>
+ * 输出: [1,2] <p>
+ * <p>
+ * 示例 2: <p>
+ * 输入: nums = [1], k = 1 <p>
+ * 输出: [1] <p>
+ * <p>
+ * 提示：
+ * 1 <= nums.length <= 105 <p>
+ * k 的取值范围是 [1, 数组中不相同的元素的个数] <p>
+ * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的 <p>
+ * <p>
+ * 进阶：你所设计算法的时间复杂度 必须 优于 O(n log n) ，其中 n 是数组大小。
+ */
 public class TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
         if (nums.length < 2) {
@@ -46,4 +67,52 @@ public class TopKFrequentElements {
         return result;
     }
 
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2)));
+    }
+
+    static class Solution {
+        public int[] topKFrequent(int[] nums, int k) {
+            if (nums.length <= 1) return nums;
+            // nums = [1,1,1,2,2,3], k = 2 -> [0,0,0,0] [0,3,2,1] => 1-3,2-2,3-1
+            // rest = [1,2] -> [1(3), 2(2), 3(1)] -k-> [1,2]
+            int min = 0;
+            int max = 0;
+            for (int n : nums) {
+                min = Math.min(min, n);
+                max = Math.max(max, n);
+            }
+            int[] table = new int[max - min + 1];
+            for (int n : nums) {
+                table[max-n]++; // [0,3,2,1]
+            }
+
+            List[] descSort = new List[nums.length];
+            for (int i = 0; i < table.length; i++) {
+                int count = table[i];
+                int num = max - i;
+
+                if (descSort[count] == null) descSort[count] =  new ArrayList<Integer>();
+                descSort[count].add(num); // 可能存在相同 count 的 num, 所以这里没有选择kv
+            }
+
+            int[] result = new int[k];
+            int index = 0;
+            for (int i=descSort.length-1; i>=0; i--) {
+                if (descSort[i] != null) {
+                    List<Integer> ns = descSort[i];
+                    for (int n : ns) {
+                        result[index++] = n;
+                        if (index == k)  return result;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    static class KV {
+        int num;
+        public KV(int num){this.num = num;}
+    }
 }
