afdd77abe827aed6c213b428a5773ca3a73ac454
leo9827 <741829374@qq.com>
2024-01-18 22:24:34 +0800

    update solution

--- src/main/java/neetcode150/slidingwindow/LongestRepeatingCharacterReplacement.java
+++ src/main/java/neetcode150/slidingwindow/LongestRepeatingCharacterReplacement.java
@@ -1,5 +1,31 @@
 package neetcode150.slidingwindow;
 
+/**
+ * <a href="https://leetcode.cn/problems/longest-repeating-character-replacement/description/">424. 替换后的最长重复字符</a>
+ * 中等 <p>
+ * 给你一个字符串 s 和一个整数 k 。你可以选择字符串中的任一字符，并将其更改为任何其他大写英文字符。该操作最多可执行 k 次。 <p>
+ * <p>
+ * 在执行上述操作后，返回 包含相同字母的最长子字符串的长度。 <p>
+ * <p>
+ * 示例 1： <p>
+ * 输入：s = "ABAB", k = 2  <p>
+ * 输出：4 <p>
+ * 解释：用两个'A'替换为两个'B',反之亦然。 <p>
+ * <p>
+ * 示例 2： <p>
+ * 输入：s = "AABABBA", k = 1 <p>
+ * 输出：4 <p>
+ * <p>
+ * 解释： <p>
+ * 将中间的一个'A'替换为'B',字符串变为 "AABBBBA"。 <p>
+ * 子串 "BBBB" 有最长重复字母, 答案为 4。 <p>
+ * 可能存在其他的方法来得到同样的结果。 <p>
+ * <p>
+ * 提示：
+ * 1 <= s.length <= 105
+ * s 仅由大写英文字母组成
+ * 0 <= k <= s.length
+ */
 public class LongestRepeatingCharacterReplacement {
 
     public int characterReplacement(String s, int k) {
@@ -21,6 +47,7 @@ public class LongestRepeatingCharacterReplacement {
         }
         return maxWindowSize;
     }
+
     public int max(int a, int b) {
         if (a > b) return a;
         return b;
@@ -28,5 +55,29 @@ public class LongestRepeatingCharacterReplacement {
 
     public static void main(String[] args) {
         System.out.println(new LongestRepeatingCharacterReplacement().characterReplacement("AABABBA", 1));
+    }
+
+    static class Solution {
+
+        public int characterReplacement(String s, int k) {
+            int winbeg = 0;
+            int maxwin = 0;
+            int[] count = new int[26];
+            int curWinMaxCount = 0;
+            for (int winend = 0; winend < s.length(); winend++) {
+                char c = s.charAt(winend);
+                count[c - 'A']++; // [1]
+
+                curWinMaxCount = Math.max(curWinMaxCount, count[c - 'A']);
+                while ((winend - winbeg + 1) - curWinMaxCount > k) { // (winend-winbeg+1)==wingap
+                    count[s.charAt(winbeg) - 'A']--; // move winbeg and remove count
+                    winbeg++;
+                }
+                int wingap = winend - winbeg + 1;
+                maxwin = Math.max(maxwin, wingap);
+            }
+
+            return maxwin;
+        }
     }
 }
