5836186aa683fc01ccf83c249cbd60da7e4e4fb3
leo9827 <741829374@qq.com>
2024-01-22 23:16:21 +0800

    update solution

---
+++ src/main/java/neetcode150/stack/ListNode.java
@@ -0,0 +1,18 @@
+package neetcode150.stack;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
